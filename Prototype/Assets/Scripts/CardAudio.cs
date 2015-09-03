4811a27f5f1fd29958b7af707939628cc841aeb6
deadshotversion2 <bernard.castello@yahoo.com>
2015-09-03 17:36:27 -0500

    The Complete Audio function
    Here's the Audio for Picking up and Dropping the cards.

---
+++ Prototype/Assets/Scripts/CardAudio.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardAudio : MonoBehaviour
+{
+    public GameObject card;
+    public AudioClip CardPickUp;
+    public AudioClip CardDrop;
+
+    private AudioSource source;
+    private AudioSource source1;
+
+
+	void Start ()
+    {
+        source = GetComponent<AudioSource>();
+        source1 = GetComponent<AudioSource>();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            float vol = 1.0f;
+            source.PlayOneShot(CardPickUp, vol);            
+        }
+
+        else if(Input.GetButtonUp("Fire1"))
+        {
+            float vol = 1.0f;
+            source1.PlayOneShot(CardDrop, vol);
+        }
+	}
+}
